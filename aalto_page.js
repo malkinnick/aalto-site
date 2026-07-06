c12d9c11c3bd6e515653ebba80f1ba6bed6021bb
Nick <108866101+malkinnick@users.noreply.github.com>
2026-07-06 17:14:39 +0200

    Bump aalto_page.js to v41d (full-width vineyard banner in Our Products flow popup); force fresh Pages deploy past stuck queue

--- aalto_page.js
+++ aalto_page.js
@@ -1,4 +1,4 @@
-window.__aaltoVer = 'v41c-products-popup-flow-img';
+window.__aaltoVer = 'v41d-products-popup-flow-img';
 /* tilda-blocks-page64821793.min.js (page block library: t1093 popups, t450 menu, t702) */
 window.isMobile=!1;if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){window.isMobile=!0}
 window.isiOS=!1;if(/iPhone|iPad|iPod/i.test(navigator.userAgent)){window.isiOS=!0}
