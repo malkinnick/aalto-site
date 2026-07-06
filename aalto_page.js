28dcbcb52ae18081f6a16fee0c993186e70d3c63
Nick <108866101+malkinnick@users.noreply.github.com>
2026-07-06 15:58:06 +0200

    Deploy v40 - Products popup flow (fresh commit to bypass stuck deploy)

--- aalto_page.js
+++ aalto_page.js
@@ -1,4 +1,4 @@
-window.__aaltoVer = 'v39-products-popup-flow';
+window.__aaltoVer = 'v40-products-popup-flow';
 /* tilda-blocks-page64821793.min.js (page block library: t1093 popups, t450 menu, t702) */
 window.isMobile=!1;if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){window.isMobile=!0}
 window.isiOS=!1;if(/iPhone|iPad|iPod/i.test(navigator.userAgent)){window.isiOS=!0}
